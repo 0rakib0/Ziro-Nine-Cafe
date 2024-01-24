16a8f86ef84969605291badbb9bc8058c84ac348
rakib <hassanrakibul926@gmail.com>
2024-01-24 14:18:50 +0600

    category section design

--- src/Pagase/HomePages/FooMenu/FoodMenu.jsx
+++ src/Pagase/HomePages/FooMenu/FoodMenu.jsx
@@ -1,10 +1,11 @@
+import FoodMenuSection from "../../../SharePage/FoodMenuSection/FoodMenuSection"
 import SectionTitle from "../../../SharePage/SectionTitle/SectionTitle"
 
 const FoodMenu = () =>{
     return (
         <div className="mt-24">
             <SectionTitle hero='Food Menu' title='Most Popular Items'></SectionTitle>
-            <h1>Food Menu Comming Soon-------</h1>
+            <FoodMenuSection></FoodMenuSection>
         </div>
     )
 }
---
+++ src/SharePage/FoodMenuSection/FoodMenuSection.jsx
@@ -0,0 +1,39 @@
+import { FaCoffee, FaHamburger, FaUtensils } from "react-icons/fa";
+
+const FoodMenuSection = () => {
+    return (
+        <div>
+            <div className="flex itens-center justify-center gap-6 my-16">
+                <div className="flex items-center gap-4 border-b-2 border-secondarycolor pb-2">
+                    <div>
+                        <FaCoffee className="text-5xl text-secondarycolor"></FaCoffee>
+                    </div>
+                    <div>
+                        <small className="text-gray-600">Popular</small>
+                        <h4 className="font-semibold">Breackfast</h4>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 border-b-2 border-secondarycolor pb-2">
+                    <div>
+                        <FaHamburger className="text-5xl text-secondarycolor"></FaHamburger>
+                    </div>
+                    <div>
+                        <small className="text-gray-600">Speacial</small>
+                        <h4 className="font-semibold">Launch</h4>
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 border-b-2 border-secondarycolor pb-2">
+                    <div>
+                        <FaUtensils className="text-5xl text-secondarycolor"></FaUtensils>
+                    </div>
+                    <div>
+                        <small className="text-gray-600">Lovely</small>
+                        <h4 className="font-semibold">Dinner</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FoodMenuSection
